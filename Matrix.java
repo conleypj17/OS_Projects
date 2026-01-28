abf676187b98fe6fa2faf27e6cc6e31b460edb70
Patrick Conley <conleypj17@gmail.com>
2026-01-28 14:53:44 -0500

    Add files via upload
    starter files

---
+++ Matrix.java
@@ -0,0 +1,101 @@
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matrix {
+ int rows; // Define the number of rows
+ int cols; // Define the number of columns
+ double values[][];
+ 
+ Random rand = new Random();
+
+ /*First constructor: with row and column as the input that creates a matrix with the specified size and 
+  * assign each elements with randomly generated number*/
+ Matrix(int r, int c) {
+  rows = r;
+  cols = c;
+  values = new double[r][c];
+  
+  for(int y = 0; y < rows; y++)
+  {
+   for(int x = 0; x < cols; x++)
+   {
+    values[y][x] = rand.nextDouble() * 10.0; //generating a double number between 0 and 10
+   }
+  }
+
+ }
+
+ /*Second constructor: with row, column, and a 2D array as the input. Similar to the first constructor
+  * above, but instead of randomly generating, it assigns the elements with the third argument double 2D array.  */
+ Matrix(int r, int c, double v[][]) {
+  
+  //Implementation here...
+	 rows = r;
+	 cols = c;
+	 values = new double[r][c];
+	 for (int y = 0; y < rows, ++y)
+	 {
+		 for (int x = 0; x < cols; ++x)
+		 {
+			 values[y][x] = v[x][y];
+		 }
+	 }
+  
+ }
+ 
+ 
+ /*Output the matrix to the console*/
+ void print()
+ {
+  for(int y = 0; y < rows; y++)
+  {
+   for(int x = 0; x < cols; x++)
+   {
+    System.out.print(values[y][x] + ", ");
+   }
+   System.out.println();
+  } 
+ }
+ 
+ 
+ /*Matrix product without thread: let the current matrix times the input argument matrix m
+  * and return the result matrix
+  * Below the multiplication is already provided but you need to add a few lines of code to 
+  * do the dimension check. If the input matrix does not satisfy the dimension requirement for
+  * matrix product, you can print out a message and return a null matrix*/
+ Matrix multiplyBy(Matrix m)
+ {
+  //Implementation here...
+  Matrix result; //You need to initialize the result matrix properly
+  
+  
+  return result;
+ }
+ 
+ 
+ /*Implementation: instead using loops above to calculate each elements, 
+  * here you will use threads to accomplish the matrix product task.
+  * Similar to the "multiplyBy()" above, the input matrix m represents
+  * the second matrix that you will use the current matrix to times. The
+  * returned Matrix will be the product result matrix.  
+  * The code below is just an example, which is not the real solution. 
+  * You need to create multiple threads to do the multiplication with each thread
+  * computing one column within the result matrix*/
+ Matrix multiplyByThreads(Matrix m)
+ {
+  //Implementation here...
+  Matrix result;//You need to initialize the result matrix properly
+  
+  return result; 
+ }
+ 
+ 
+ /* The main function for evaluation purpose*/
+ public static void main(String[] args)
+ {
+  //Implementation here...
+  
+  
+ }
+}
